32b2c481432c2a19d4b8b943302cd092d30b1efe
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-10-26 13:06:05 -0700

    Initial Commit

---
+++ TwitterCF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterCF
+//
+//  Created by Lindsey on 10/26/15.
+//  Copyright © 2015 Lindsey Boggio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
