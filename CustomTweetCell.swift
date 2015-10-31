b920e854bb7342936178a6d0363f34f4315d6559
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-10-31 07:21:05 -0700

    Start of Thursday homework

--- CustomTweetCell.swift
+++ CustomTweetCell.swift
@@ -6,34 +6,36 @@
 //  Copyright © 2015 Lindsey Boggio. All rights reserved.
 //
 
-//import UIKit
-//
-//class CustomTweetCell: UITableViewCell {
-//    //insert outlets here
-//    
-//    
-//    var tweet: Tweet? {
-//        didSet {
-//            if let tweet = self.tweet, user = tweet.user, text = self.tweet?.text{
-//                self.tweetTextLabel.text = text
-//                
-//                if let image = user.image{
-//                    self.imgView.image = image
-//                } else {
-//                    if let url = NSURL(string: user.profileImage){
-//                        let downloadQ = dispatch_queue_create("downloadQ", nil)
-//                            dispatch_async(downloadQ, { () -> Void in
-//                                let imageData = NSData(contentsOfURL: url)!
-//                                
-//                                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//                                    let image = UIImage (data: imageData)
-//                                    
-//                            })
-//                    }
-//                }
-//            }
-//        }
-//    }
-//    
-//    
-//}
+import UIKit
+
+class CustomTweetCell: UITableViewCell {
+    //insert outlets here
+    
+    @IBOutlet weak var imgView: UIButton!
+    
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweet: Tweet? {
+        didSet {
+            if let tweet = self.tweet, user = tweet.user, text = self.tweet?.text{
+                self.tweetTextLabel.text = text
+                
+                if let image = user.image{
+                    self.imgView = 
+                } else {
+                    if let url = NSURL(string: user.profileImage){
+                        let downloadQ = dispatch_queue_create("downloadQ", nil)
+                            dispatch_async(downloadQ, { () -> Void in
+                                let imageData = NSData(contentsOfURL: url)!
+                                
+                                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                    let image = UIImage (data: imageData)
+                                    self.imgView.image = image
+                                    user.image = image
+                                    })
+                                })
+                            }
+                        }
+                    }
+                }
+            }
